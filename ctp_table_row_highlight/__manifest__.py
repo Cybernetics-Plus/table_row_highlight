22f65b12457915bb259923fede58518aa29aecf3
BOMEN <bomen21x@gmail.com>
2022-11-21 01:19:29 +0700

    Table

--- ctp_table_row_highlight/__manifest__.py
+++ ctp_table_row_highlight/__manifest__.py
@@ -7,31 +7,33 @@
 ###################################################################################
 
 {
-    'name': 'Table Row Hover And Selected Highlight',
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "name": "Table Row Hover And Selected Highlight",
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Cybernetics Plus Tools Table Row Hover And Selected Highlight
             .""",
-    'description': """ 
+    "description": """ 
             Cybernetics Plus Tools Table Row Hover And Selected Highlight
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Tools',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'depends': ['web'],
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "depends": ["web"],
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'ctp_table_row_highlight/static/src/css/style.css',
-            'ctp_table_row_highlight/static/src/js/web.js',
+    "assets": {
+        "web.assets_backend": [
+            "ctp_table_row_highlight/static/src/css/style.css",
+            "ctp_table_row_highlight/static/src/js/web.js",
         ],
     }
 }
